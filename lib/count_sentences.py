5ee8bbfdbcd6309fe1ca31d79f3fb08662dcddd2
isaacmogaka <isaacmogaka2016@gmail.com>
2023-08-22 12:30:46 +0300

    Completed Assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -2,3 +2,19 @@
 
 class MyString:
   pass
+class MyString:
+    def __init__(self, value):
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        sentences = self.value.split('.')
+        return len(sentences)
